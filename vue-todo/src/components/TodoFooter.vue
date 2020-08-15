1b4ced29afa64d27c6bbfba1ffe0307e4afa869d
areum514 <21500514@handong.edu>
2020-08-15 15:44:48 +0900

    TodoFooter 컴포넌트 구현

--- vue-todo/src/components/TodoFooter.vue
+++ vue-todo/src/components/TodoFooter.vue
@@ -1,15 +1,37 @@
 <template>
   <div>
-      footer
+      <div class="clearAllContainer"
+      v-on:click="clearTodo">
+        <span class="clearAllBtn">
+          clear all
+        </span>
+      </div>
   </div>
 </template>
 
 <script>
 export default {
-
+methods:{
+  clearTodo:function(){
+    localStorage.clear()
+  }
+}
 }
 </script>
 
-<style>
+<style scoped>
+.clearAllContainer{
+  width: 8.5rem;
+  height: 50px;
+  line-height: 50px;
+  background-color: white;
+  border-radius: 5px;
+  margin: 0 auto;
+}
+.clearAllBtn{
+  color: #E20303;
+  display: block;
+}
+
 
 </style>
