c8bcfd90a873b52a808b4946d1aa7c4effb50555
areum514 <21500514@handong.edu>
2020-08-17 12:40:43 +0900

    트랜지션 구현

--- vue-todo/src/components/TodoList.vue
+++ vue-todo/src/components/TodoList.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <ul>
+     <transition-group name="list" tag="ul">
       <li v-for="(todoItem,index) in propsdata" v-bind:key="todoItem" class="shodow">
         <i class="fas fa-check checkBtn " 
           v-on:click="togleComplete(todoItem,index)"
@@ -12,7 +12,7 @@
           </span>
         
       </li>
-    </ul>
+    </transition-group>
   </div>
 </template>
 
@@ -26,7 +26,6 @@ export default {
     },
     togleComplete:function(todoItem,index){
       this.$emit('toggleItem',todoItem,index)
-       
     }
   },
  
@@ -68,5 +67,13 @@ li{
   text-decoration: line-through;
   color: #B3ADAD;
 }
+/* 리스트 아이템 트랜지션 효과 */
 
+.list-enter-active, .list-leave-active {
+  transition: all 1s;
+}
+.list-enter, .list-leave-to /* .list-leave-active below version 2.1.8 */ {
+  opacity: 0;
+  transform: translateY(30px);
+}
 </style>
